ed2b9f2b5c2036bfa4c987e52aa6b3ecfa872acc
DanilBych <149468036+DanilBych@users.noreply.github.com>
2024-05-22 20:58:17 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/MyProject/ui/MainActivity.java
+++ app/src/main/java/com/example/MyProject/ui/MainActivity.java
@@ -1,30 +1,22 @@
 package com.example.MyProject.ui;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.MyProject.R;
-import com.example.MyProject.data.MyNotesDatabase;
 import com.example.MyProject.data.Product;
 import com.example.MyProject.databinding.ActivityMainBinding;
 import com.example.MyProject.ui.mynotes.MyNotesActivity;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Calendar;
 import java.util.LinkedList;
@@ -62,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
                 tempList = products;
                 arrayAdapter.setProductList(products);
                 if(threadflag) {
-                    Toast.makeText(MainActivity.this, R.string.started_update, Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, R.string.started_update,
+                            Toast.LENGTH_LONG).show();
                     update = new Thread(new AddNewProduct( viewModel, tempList, sp));
                     update.start();
                     threadflag = false;
@@ -83,7 +76,8 @@ public class MainActivity extends AppCompatActivity {
             public void onProductClick(int position) {
                 Product product = arrayAdapter.getProductList().get(position);
 
-                Intent productIntent = new Intent(MainActivity.this, ProductActivity.class);
+                Intent productIntent = new Intent(MainActivity.this,
+                        ProductActivity.class);
                 productIntent.putExtra("Title", product.getTitle());
                 productIntent.putExtra("Proven", product.getProven());
                 productIntent.putExtra("Url", product.getUrl());
@@ -110,7 +104,8 @@ public class MainActivity extends AppCompatActivity {
                     for (int i = 0; i < tempList.size(); i++) {
                         if(tempList.get(i).getTitle().length() > j + str.length()){
                             flag = true;
-                            if (tempList.get(i).getTitle().substring(j, j + str.length()).toLowerCase().equals(str)) searchList.add(tempList.get(i));
+                            if (tempList.get(i).getTitle().substring(j, j + str.length())
+                                    .toLowerCase().equals(str)) searchList.add(tempList.get(i));
                         }
                     }
                     j++;
@@ -126,24 +121,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(
-                new ItemTouchHelper.SimpleCallback(0,
-                        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-                    @Override
-                    public boolean onMove(@NonNull RecyclerView recyclerView,
-                                          @NonNull RecyclerView.ViewHolder viewHolder,
-                                          @NonNull RecyclerView.ViewHolder target) {
-                        return false;
-                    }
-
-                    @Override
-                    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-                        int position = viewHolder.getAdapterPosition();
-                        Product product = arrayAdapter.getProductList().get(position);
-                        showAlert(product);
-
-                    }
-                });
 
 
         binding.btnMyNotes.setOnClickListener(new View.OnClickListener() {
@@ -153,84 +130,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        Log.d("NNN", String.valueOf(viewModel));
 
     }
 
 
-    public void showAlert(Product product) {
-        new AlertDialog.Builder(com.example.MyProject.ui.MainActivity.this)
-                .setTitle("Удаление элемента")
-                .setMessage("Удалить элемент " + product.getTitle() + "?")
-                .setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        viewModel.remove(product);
-
-                    }
-                })
-                .setNeutralButton("EDIT", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-//                        Intent intent = AddNewProductActivity.newIntent(MainActivity.this,
-//                                String.valueOf(product.getId()),
-//                                product.getTitle(),
-//                                product.getProven(),
-//                                product.getPrice());
-//
-//                        startActivity(intent);
-                    }
-                })
-                .setNegativeButton(android.R.string.no, null)
-                .setIcon(android.R.drawable.ic_delete)
-                .show();
-    }
-
-
-//    private void addSortList() {
-//
-//        binding.btnSort.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//
-//              /*  new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//
-//                        tempList.sort(new Comparator<Product>() {
-//                            @Override
-//                            public int compare(Product o1, Product o2) {
-//                                return o2.getTitle().compareTo(o1.getTitle());
-//                            }
-//                        });
-//                        handler.post(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                arrayAdapter.setProductList(tempList);
-//                            }
-//                        });
-//
-//                    }
-//                }).start();*/
-//
-//
-//            }
-//        });
-//
-//    }
-
-//    private void addListData() {
-//        binding.btnAdd.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = AddNewProductActivity.newIntent(MainActivity.this,
-//                        String.valueOf(0), "", "", "");
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-//    }
 
     @Override
     protected void onResume() {
